72fe016eb6c0bf011f5b6861716e1bb38e9999a5
Kobe Vandenberghe <kobe.vandenberghe@live.com>
2023-04-08 17:45:50 +0200

    Player input added

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,7 +1,14 @@
 ﻿// Speler == X en computer == O
 using TicTacToe;
 
-Grid grid = new Grid();
+string PositieInvoer;
+bool Spelen = true;
 
-Console.WriteLine(grid.GridPrinten());
-Console.ReadLine();
+Grid grid = new Grid();
+while (Spelen == true)
+{
+    Console.WriteLine(grid.GridPrinten());
+    Console.WriteLine("In welk vakje Wil je uw X zetten ?");
+    PositieInvoer = Console.ReadLine();
+    grid.GridInvullenSpeler(PositieInvoer.Substring(0, 1), Convert.ToInt16(PositieInvoer.Substring(1, 1)));
+}
